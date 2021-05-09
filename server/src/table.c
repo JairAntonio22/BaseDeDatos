249e4fdd1271ea2f9c4afddf55f7e6b3bfec6384
JairAntonio22 <jair.loranca@gmail.com>
2021-05-08 21:39:16 -0500

    Actualización de base de datos

--- server/src/table.c
+++ server/src/table.c
@@ -340,37 +340,17 @@ char* encode_table(Table *table) {
     }
 
     char *msg = (char*) calloc(sizeof(char), 2000);
-    char *buffer = (char*) calloc(sizeof(char), 100);
-    char separator[1] = ",";
-    char eof[1] = "\0";
+    char separator[] = ",";
 
-    strcat(msg, table->name);
-    strcat(msg, separator);
-
-    sprintf(buffer, "%i", table->rows);
-    strcat(msg, buffer);
-    strcat(msg, separator);
-
-    sprintf(buffer, "%i", table->cols);
-    strcat(msg, buffer);
-    strcat(msg, separator);
-
-    free(buffer);
-    
-    for (int i = 0; i < table->rows - 1; i++) {
-        for (int j = 0; j < table->cols; j++) {
+    for (int i = 0; i < table->rows; i++) {
+        for (int j = 0; j < table->cols - 1; j++) {
             strcat(msg, table->data[i][j]);
             strcat(msg, separator);
         }
-    }
 
-    for (int j = 0; j < table->cols - 1; j++) {
-        strcat(msg, table->data[table->rows - 1][j]);
-        strcat(msg, separator);
+        strcat(msg, table->data[i][table->cols - 1]);
+        strcat(msg, "\n");
     }
-
-    strcat(msg, table->data[table->rows - 1][table->cols - 1]);
-    strcat(msg, eof);
 
     return msg;
 }
