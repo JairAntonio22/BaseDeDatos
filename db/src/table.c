3733ad5be034b36626f8c59c7c3a8f2077e717a0
JairAntonio22 <jair.loranca@gmail.com>
2021-05-09 20:56:21 -0500

    Validación de where en select_table

--- db/src/table.c
+++ db/src/table.c
@@ -165,7 +165,7 @@ Table* select_table(Table *table, int ncols, char **cols, char **where) {
         return NULL;
     }
 
-    bool valid = true;
+    bool valid = false;
 
     for (int i = 0; i < ncols; i++) {
         for (int j = 0; j < table->cols; j++) {
@@ -178,6 +178,17 @@ Table* select_table(Table *table, int ncols, char **cols, char **where) {
             return NULL;
         }
     }
+
+    for (int j = 0; j < table->cols; j++) {
+        if (strcmp(where[0], table->data[0][j]) == 0) {
+            valid = true;
+        }
+    }
+
+    if (!valid) {
+        return NULL;
+    }
+
 
     bool *mask = (bool*) calloc(sizeof(bool), table->cols);
     int *map = (int*) calloc(sizeof(bool), table->cols);
