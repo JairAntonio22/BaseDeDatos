517a4a5904cd6568b17c51d176ebb8a4c25d26b4
JairAntonio22 <jair.loranca@gmail.com>
2021-05-09 21:45:26 -0500

    Corección de select_table

--- db/src/table.c
+++ db/src/table.c
@@ -179,16 +179,18 @@ Table* select_table(Table *table, int ncols, char **cols, char **where) {
         }
     }
 
-    valid = false;
+    if (where != NULL) {
+        valid = false;
 
-    for (int j = 0; j < table->cols; j++) {
-        if (strcmp(where[0], table->data[0][j]) == 0) {
-            valid = true;
+        for (int j = 0; j < table->cols; j++) {
+            if (strcmp(where[0], table->data[0][j]) == 0) {
+                valid = true;
+            }
         }
-    }
 
-    if (!valid) {
-        return NULL;
+        if (!valid) {
+            return NULL;
+        }
     }
 
 
